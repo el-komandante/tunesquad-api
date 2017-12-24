4ae248a238bd9e6aedaa79c492c1c2bac7f0a6b0
el-komandante <rudydeberry@hotmail.com>
2017-12-24 12:34:48 -0800

    change queue route, add user route

--- api/api.py
+++ api/api.py
@@ -2,10 +2,12 @@
 from aiohttp import web
 from .search import search_handler
 from .session import sessions_handler
-# from .queue import queue_handler
+from .queue import queue_handler
+from .user import user_handler
 app = web.Application()
 
 # app.router.add_options()
 app.router.add_get('/search', search_handler)
 app.router.add_post('/sessions', sessions_handler)
-# app.router.add_put('/queue/{session_id}', queue_handler)
+app.router.add_put('/sessions/{session_id}', user_handler)
+app.router.add_put('/sessions/{session_id}/queue', queue_handler)
--- app/session/new_session_id.py
+++ app/session/new_session_id.py
@@ -1,6 +1,6 @@
 import hashlib
 import base64
+import random
 
 def new_session_id():
-    hash_input = 'plunkbat'.encode('utf-8')
-    return base64.urlsafe_b64encode(hashlib.md5(hash_input).digest()).decode()[:10]
+    return hashlib.sha256(str(random.getrandbits(256)).encode('utf-8')).hexdigest()[:10]
