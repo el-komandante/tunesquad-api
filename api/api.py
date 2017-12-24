ad8800feaeb78f5fd17558c3f5c46d581ac47824
Rashad Laher <rashadlaher@gmail.com>
2017-12-24 15:37:07 -0500

    added User class and started websocket

--- api/api.py
+++ api/api.py
@@ -1,7 +1,8 @@
-
 from aiohttp import web
 from .search import search_handler
+from .websocket import websocket_handler
 
 app = web.Application()
 
 app.router.add_get('/search', search_handler)
+app.router.add_get('/ws', websocket_handler)
